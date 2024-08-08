5c64a134b19a658ec3e8437415883f2759d53c45
AlinBorcea <52531291+AlinBorcea@users.noreply.github.com>
2024-08-08 12:19:25 +0300

    added sort strategies

--- src/main/scala/Table.scala
+++ src/main/scala/Table.scala
@@ -1,4 +1,5 @@
 import dice.DieFace
+import dice.DieFace.*
 
 class Table(val reorderStrategy: List[DieFace] => List[DieFace],
             var dice: List[DieFace] = List()) {
@@ -12,4 +13,25 @@ object Table {
   def create(reorderStrategy: List[DieFace] => List[DieFace]): Table = {
     Table(reorderStrategy)
   }
+  
+  def sortDefenseFirst(dice: List[DieFace]): List[DieFace] = {
+    val axes = dice.filter(_ == Ax)
+    val arrows = dice.filter(_ == Arrow)
+    val helmets = dice.filter(_ == Helmet)
+    val shields = dice.filter(_ == Shield)
+    val hands = dice.filter(_ == Hand)
+
+    helmets ::: shields ::: axes ::: arrows ::: hands
+  }
+  
+  def sortAttackFirst(dice: List[DieFace]): List[DieFace] = {
+    val axes = dice.filter(_ == Ax)
+    val arrows = dice.filter(_ == Arrow)
+    val helmets = dice.filter(_ == Helmet)
+    val shields = dice.filter(_ == Shield)
+    val hands = dice.filter(_ == Hand)
+
+    axes ::: arrows ::: helmets ::: shields ::: hands
+  }
+  
 }

1d386e280ab4ed6797e50aee093ee2af39e8cdb7
AlinBorcea <52531291+AlinBorcea@users.noreply.github.com>
2024-08-08 11:52:46 +0300

    added class Table

--- src/main/scala/Player.scala
+++ src/main/scala/Player.scala
@@ -1,8 +1,10 @@
-class Player:
+class Player(val table: Table) {
   private var health = 15
+  val bowl = Bowl()
 
   def decreaseHealth(amount: Int): Unit = {
     health -= amount
   }
-  
+
   def isHealthEmpty: Boolean = health <= 0
+}
